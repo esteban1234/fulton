fbe35807dbb2f73621b3b4abfb3c032a4fa270f2
esteban1234 <tva_libra@hotmail.com>
2018-03-26 18:54:28 -0600

    a la mitad

---
+++ index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <title>FULTON TEXTILES</title>
+   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
+    <META NAME="Title" content="FULTON TEXTILES">
+    <META NAME="Author" CONTENT="[http://textilesfulton.com/]"/>
+    <META name="Description" content=""/>
+     <META NAME="Keywords" CONTENT=""/>
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/iconos.min.css">
+    <link rel="stylesheet" href="css/fontawesome-all.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/responsivo.css">
+    <link href="https://fonts.googleapis.com/css?family=Khula|Montserrat|Roboto" rel="stylesheet">
+    <script src="js/general.js"></script>
+
+
+
+    <script src="js/smooth-scroll.min.js"></script>
+    <script>
+      smoothScroll.init({
+      selector: '[data-scroll]',
+      selectorHeader: null,
+      speed: 2000,
+      easing: 'easeInOutCubic',
+      offset: 0,
+      callback: function ( anchor, toggle ) {}
+      });
+    </script>
+
+  </head>
+  <body>
+
+    <div class="menu" id="INICIO">
+        <nav class="pull-right">
+          <ul>
+            <a class="selector" id="select" data-scroll href="#INICIO" aria-pressed="true">INICIO</a>
+            <a class="selector" data-scroll href="#NOSOTROS" aria-pressed="true">NOSOTROS</a>
+            <a class="selector" data-scroll href="#SERVICIOS" aria-pressed="true">PRODUCTOS</a>
+            <a class="selector" data-scroll href="#CONTACTO" aria-pressed="true">CONTACTO</a>
+          </ul>
+        </nav>
+
+
+        <figure class="logo pull-left">
+          <img src="img/fulton.png" alt="FULTON" class="img-responsive">
+        </figure>
+      </div>
+
+    <div class="banner">
+        <div class="fondo">
+          <div class="textobanner">
+            LA MEJOR EXPERIENCIA TEXTIL
+          </div>
+          <div class="container">
+  <div class="row">
+
+        <div class="boton">
+            <a data-scroll href="#CONTACTO" aria-pressed="true" class="btn btn-outlined btn-block btn-danger">CONTACTANOS</a>
+        </div>
+  </div>
+</div>
+        </div>
+    </div>
+
+
+
+  <span class="ir-arriba"><i class="fa fa-arrow-up" aria-hidden="true"></i></span>
+
+<section class="seccion1" id="NOSOTROS" >
+ <div class="section" style="padding: 5% 0%;">
+      <div class="container">
+        <h1>FULTON TEXTILES SA DE CV</h1>
+        <h2></h2>
+        <p class="texto">Somos una empresa especialista en la fabricación de maquinaria textil para distintos campos: SALA DE CORTE, LAVADO y TEÑIDO de prendas, PLANCHADO para género de punto y SERIGRAFÍA textil. <br><br> 
+        Dedicamos gran parte de nuestros recursos a desarrollar nuevos proyectos a partir de las tecnologías más avanzadas, respondiendo a las exigencias de nuestros clientes en cuanto a mejoras en la productividad y la calidad de sus acabados textiles. <br><br>
+
+        Nuestra potencia innovadora y un equipo formado por profesionales con conocimientos técnicos avanzados, nos permite vender nuestra amplia gama de equipamientos a través de nuestra red comercial. </p>
+        <br><br>
+        <div class="row">
+          <div class="col-md-4">
+            <figure class="img1">
+              
+            </figure>
+          </div>
+          <div class="col-md-4">
+            <figure class="img2">
+              
+            </figure>
+          </div>
+          <div class="col-md-4">
+            <figure class="img3">
+              
+            </figure>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+
+<section class="seccion2" id="SERVICIOS" style="background: #BF1724;">
+  <div class="section" style="padding: 5% 0%;">
+      <div class="container">
+        <h1 style="color: #fff;">PRODUCTOS</h1>
+        <p style="color: #fff;">LÍDER EN FABRICACIÓN DE MÁQUINAS PARA CORTE, SOLDADURA, COSTURA Y MANIPULACIÓN TEXTIL</p> <br><br>
+        <div class="row">
+          <div class="col-md-3">
+            <figure class="imgp1">
+              
+            </figure>
+            <h2 style="color: #fff;">TOLDO</h2>
+            <p style="color: #fff;">Soluciones para la confección en tejidos acrílicos, poliéster, screens, PVC y otros de toldo, toldo de brazo invisible, toldo vertical, cofre, punto recto, pérgola, toldo screen, toldo doble, marquesina, capota, parasol, vela tensada, velum, etc.</p>
+          </div>
+          <div class="col-md-3">
+            <figure class="imgp2">
+              
+            </figure>
+            <h2 style="color: #fff;">CORTINA TÉCNICA</h2>
+            <p style="color: #fff;">Soluciones para la confección de tejidos técnicos (screens, black-out, Zebra u otros) para cortina vertical, cortina enrollable, panel japonés, cortina veneciana, estor, cortina con lamas, etc.</p>
+          </div>
+          <div class="col-md-3">
+            <figure class="imgp3">
+              
+            </figure>
+            <h2 style="color: #fff;">CORTINA TRADICIONAL</h2>
+            <p style="color: #fff;">Soluciones para la confección de tejidos para estor pacchetto, cortina tradicional, estor romana, panel japonés, estor plegable, etc.</p>
+          </div>
+          <div class="col-md-3">
+            <figure class="imgp4">
+              
+            </figure>
+            <h2 style="color: #fff;">TAPICERIA</h2>
+            <p style="color: #fff;">Máquinas especificas para el sector de la tapicería de mueble.</p>
+          </div>
+        </div>
+
+        <br><br>
+        <div class="row">
+          <div class="col-md-3">
+            <figure class="imgp5">
+              
+            </figure>
+            <h2 style="color: #fff;">MARROQUINERÍA </h2>
+            <p style="color: #fff;">Máquinas especificas para el sector de la confección de bolsos, cinturones y piel.</p>
+          </div>
+          <div class="col-md-3">
+            <figure class="imgp6">
+              
+            </figure>
+            <h2 style="color: #fff;">INTERIOR AUTOMÓVILES </h2>
+            <p style="color: #fff;">Soluciones integrales para la confección de interiores de automóviles y costuras especiales (airbag).</p>
+          </div>
+          <div class="col-md-3">
+            <figure class="imgp7">
+              
+            </figure>
+            <h2 style="color: #fff;">PRENDAS DE VESTIR </h2>
+            <p style="color: #fff;">Máquinas para la industria de la confección de prendas de vestir.</p>
+          </div>
+          <div class="col-md-3">
+            <figure class="imgp8">
+              
+            </figure>
+            <h2 style="color: #fff;">OTROS TEJIDOS TÉCNICOS </h2>
+            <p style="color: #fff;">Máquinas para la fabricación de filtros industriales, alfombras, sábanas, eslingas.</p>
+          </div>
+        </div>
+
+        <br><br>
+        <div class="row">
+          
+          
+          <div class="col-md-3 col-md-offset-3">
+            <figure class="imgp9">
+              
+            </figure>
+            <h2 style="color: #fff;">VELAS </h2>
+            <p style="color: #fff;">Máquinas para la confección de cualquier tipo de vela naútica.</p>
+          </div>
+          <div class="col-md-3">
+            <figure class="imgp10">
+              
+            </figure>
+            <h2 style="color: #fff;">REDES </h2>
+            <p style="color: #fff;">Máquinas para la fabricación de redes de pesca, deportivas, protección laboral y para la acuicultura.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+
+<section class="seccion3" id="CONTACTO">
+  <section id="contact" style="padding: 5% 0%;">
+  <div class="container">
+    <div class="well well-sm">
+      <h3><strong>CONTACTO</strong></h3>
+    </div>
+  
+  <div class="row">
+    <div class="col-md-7">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d3736489.7218514383!2d90.21589792292741!3d23.857125486636733!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1506502314230" width="100%" height="315" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+
+      <div class="col-md-5">
+          <h4><strong>Get in Touch</strong></h4>
+        <form>
+          <div class="form-group">
+            <input type="text" class="form-control" name="" value="" placeholder="Name">
+          </div>
+          <div class="form-group">
+            <input type="email" class="form-control" name="" value="" placeholder="E-mail">
+          </div>
+          <div class="form-group">
+            <input type="tel" class="form-control" name="" value="" placeholder="Phone">
+          </div>
+          <div class="form-group">
+            <textarea class="form-control" name="" rows="3" placeholder="Message"></textarea>
+          </div>
+          <button class="btn btn-default" type="submit" name="button">
+              <i class="fa fa-paper-plane-o" aria-hidden="true"></i> Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+</section>
+
+
+<footer>
+<p>Derechos de autor 2018 © |  FULTON TEXTILES SA DE CV</p>
+</footer>
+
+    <script src="js/sendCORREO.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/arriba.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/menu.js"></script>
+  </body>
+</html>
