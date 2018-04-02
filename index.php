133dec8f5a36b07f7c041a520d16471ddb549e4d
champiiy <albert1047@hotmail.com>
2018-04-01 19:16:32 -0500

    ADD RESPONSIVO

--- index.php
+++ index.php
@@ -52,21 +52,24 @@
         <figure class="logo pull-left">
           <img src="img/fulton.png" alt="FULTON" class="img-responsive">
         </figure>
+
+         <div class="openclose">
+          <i class="fa fa-bars open" aria-hidden="true"></i>
+        </div>
       </div>
 
     <div class="banner">
-        <div class="fondo">
+       <div class="fondo">
           <div class="textobanner">
             LA MEJOR EXPERIENCIA TEXTIL
           </div>
           <div class="container">
-  <div class="row">
-
-        <div class="boton">
-            <a data-scroll href="#CONTACTO" aria-pressed="true" class="btn btn-outlined btn-block btn-danger">CONTACTANOS</a>
-        </div>
-  </div>
-</div>
+            <div class="row">
+              <div class="boton">
+                <a data-scroll href="#CONTACTO" aria-pressed="true" class="btn btn-outlined btn-block btn-danger">CONTACTANOS</a>
+              </div>
+            </div>
+          </div>
         </div>
     </div>
 
@@ -77,7 +80,7 @@
 <section class="seccion1" id="NOSOTROS" >
  <div class="section" style="padding: 5% 0%;">
       <div class="container">
-        <h1>FULTON TEXTILES SA DE CV</h1>
+        <h1 class="tit-ft">FULTON TEXTILES SA DE CV</h1>
         <h2></h2>
         <p class="texto">Somos una empresa especialista en la fabricación de maquinaria textil para distintos campos: SALA DE CORTE, LAVADO y TEÑIDO de prendas, PLANCHADO para género de punto y SERIGRAFÍA textil. <br><br> 
         Dedicamos gran parte de nuestros recursos a desarrollar nuevos proyectos a partir de las tecnologías más avanzadas, respondiendo a las exigencias de nuestros clientes en cuanto a mejoras en la productividad y la calidad de sus acabados textiles. <br><br>
@@ -85,17 +88,17 @@
         Nuestra potencia innovadora y un equipo formado por profesionales con conocimientos técnicos avanzados, nos permite vender nuestra amplia gama de equipamientos a través de nuestra red comercial. </p>
         <br><br>
         <div class="row">
-          <div class="col-md-4">
+          <div class="col-sm-4 col-md-4">
             <figure class="img1">
               
             </figure>
           </div>
-          <div class="col-md-4">
+          <div class="col-sm-4 col-md-4">
             <figure class="img2">
               
             </figure>
           </div>
-          <div class="col-md-4">
+          <div class="col-sm-4 col-md-4">
             <figure class="img3">
               
             </figure>
@@ -108,68 +111,68 @@
 <section class="seccion2" id="SERVICIOS" style="background: #BF1724;">
   <div class="section" style="padding: 5% 0%;">
       <div class="container">
-        <h1 style="color: #fff;">PRODUCTOS</h1>
-        <p style="color: #fff;">LÍDER EN FABRICACIÓN DE MÁQUINAS PARA CORTE, SOLDADURA, COSTURA Y MANIPULACIÓN TEXTIL</p> <br><br>
+        <h1 class="tit-prd" style="color: #fff;">PRODUCTOS</h1>
+        <p class="subtit-prd" style="color: #fff;">LÍDER EN FABRICACIÓN DE MÁQUINAS PARA CORTE, SOLDADURA, COSTURA Y MANIPULACIÓN TEXTIL</p> <br><br class="delete-br">
         <div class="row">
-          <div class="col-md-3">
+          <div class="col-sm-6 col-md-3">
             <figure class="imgp1">
-              
             </figure>
-            <h2 style="color: #fff;">TOLDO</h2>
-            <p style="color: #fff;">Soluciones para la confección en tejidos acrílicos, poliéster, screens, PVC y otros de toldo, toldo de brazo invisible, toldo vertical, cofre, punto recto, pérgola, toldo screen, toldo doble, marquesina, capota, parasol, vela tensada, velum, etc.</p>
+            <h2 class="tit-all" style="color: #fff;">TOLDO</h2>
+            <p class="subtit-all" style="color: #fff;">Soluciones para la confección en tejidos acrílicos, poliéster, screens, PVC y otros de toldo, toldo de brazo invisible, toldo vertical, cofre, punto recto, pérgola, toldo screen, toldo doble, marquesina, capota, parasol, vela tensada, velum, etc.</p>
           </div>
-          <div class="col-md-3">
+          <div class="col-sm-6 col-md-3">
             <figure class="imgp2">
               
             </figure>
-            <h2 style="color: #fff;">CORTINA TÉCNICA</h2>
-            <p style="color: #fff;">Soluciones para la confección de tejidos técnicos (screens, black-out, Zebra u otros) para cortina vertical, cortina enrollable, panel japonés, cortina veneciana, estor, cortina con lamas, etc.</p>
+            <h2 class="tit-all" style="color: #fff;">CORTINA TÉCNICA</h2>
+            <p class="subtit-all" style="color: #fff;">Soluciones para la confección de tejidos técnicos (screens, black-out, Zebra u otros) para cortina vertical, cortina enrollable, panel japonés, cortina veneciana, estor, cortina con lamas, etc.</p>
           </div>
-          <div class="col-md-3">
+          <div class="clearfix visible-sm-block"></div>
+          <div class="col-sm-6 col-md-3">
             <figure class="imgp3">
               
             </figure>
-            <h2 style="color: #fff;">CORTINA TRADICIONAL</h2>
-            <p style="color: #fff;">Soluciones para la confección de tejidos para estor pacchetto, cortina tradicional, estor romana, panel japonés, estor plegable, etc.</p>
+            <h2 class="tit-all" style="color: #fff;">CORTINA TRADICIONAL</h2>
+            <p class="subtit-all" style="color: #fff;">Soluciones para la confección de tejidos para estor pacchetto, cortina tradicional, estor romana, panel japonés, estor plegable, etc.</p>
           </div>
-          <div class="col-md-3">
+          <div class="col-sm-6 col-md-3">
             <figure class="imgp4">
               
             </figure>
-            <h2 style="color: #fff;">TAPICERIA</h2>
-            <p style="color: #fff;">Máquinas especificas para el sector de la tapicería de mueble.</p>
+            <h2 class="tit-all" style="color: #fff;">TAPICERIA</h2>
+            <p class="subtit-all" style="color: #fff;">Máquinas especificas para el sector de la tapicería de mueble.</p>
           </div>
         </div>
 
         <br><br>
         <div class="row">
-          <div class="col-md-3">
+          <div class="col-sm-6 col-md-3">
             <figure class="imgp5">
               
             </figure>
-            <h2 style="color: #fff;">MARROQUINERÍA </h2>
-            <p style="color: #fff;">Máquinas especificas para el sector de la confección de bolsos, cinturones y piel.</p>
+            <h2 class="tit-all" style="color: #fff;">MARROQUINERÍA </h2>
+            <p class="subtit-all" style="color: #fff;">Máquinas especificas para el sector de la confección de bolsos, cinturones y piel.</p>
           </div>
-          <div class="col-md-3">
+          <div class="col-sm-6 col-md-3">
             <figure class="imgp6">
               
             </figure>
-            <h2 style="color: #fff;">INTERIOR AUTOMÓVILES </h2>
-            <p style="color: #fff;">Soluciones integrales para la confección de interiores de automóviles y costuras especiales (airbag).</p>
+            <h2 class="tit-all" style="color: #fff;">INTERIOR AUTOMÓVILES </h2>
+            <p class="subtit-all" style="color: #fff;">Soluciones integrales para la confección de interiores de automóviles y costuras especiales (airbag).</p>
           </div>
-          <div class="col-md-3">
+          <div class="col-sm-6 col-md-3">
             <figure class="imgp7">
               
             </figure>
-            <h2 style="color: #fff;">PRENDAS DE VESTIR </h2>
-            <p style="color: #fff;">Máquinas para la industria de la confección de prendas de vestir.</p>
+            <h2 class="tit-all" style="color: #fff;">PRENDAS DE VESTIR </h2>
+            <p class="subtit-all" style="color: #fff;">Máquinas para la industria de la confección de prendas de vestir.</p>
           </div>
-          <div class="col-md-3">
+          <div class="col-sm-6 col-md-3">
             <figure class="imgp8">
               
             </figure>
-            <h2 style="color: #fff;">OTROS TEJIDOS TÉCNICOS </h2>
-            <p style="color: #fff;">Máquinas para la fabricación de filtros industriales, alfombras, sábanas, eslingas.</p>
+            <h2 class="tit-all" style="color: #fff;">OTROS TEJIDOS TÉCNICOS </h2>
+            <p class="subtit-all" style="color: #fff;">Máquinas para la fabricación de filtros industriales, alfombras, sábanas, eslingas.</p>
           </div>
         </div>
 
@@ -177,19 +180,19 @@
         <div class="row">
           
           
-          <div class="col-md-3 col-md-offset-3">
+          <div class="col-sm-6 col-md-3 col-md-offset-3">
             <figure class="imgp9">
               
             </figure>
-            <h2 style="color: #fff;">VELAS </h2>
-            <p style="color: #fff;">Máquinas para la confección de cualquier tipo de vela naútica.</p>
+            <h2 class="tit-all" style="color: #fff;">VELAS </h2>
+            <p class="subtit-all" style="color: #fff;">Máquinas para la confección de cualquier tipo de vela naútica.</p>
           </div>
-          <div class="col-md-3">
+          <div class="col-sm-6 col-md-3">
             <figure class="imgp10">
               
             </figure>
-            <h2 style="color: #fff;">REDES </h2>
-            <p style="color: #fff;">Máquinas para la fabricación de redes de pesca, deportivas, protección laboral y para la acuicultura.</p>
+            <h2 class="tit-all" style="color: #fff;">REDES </h2>
+            <p class="subtit-all" style="color: #fff;">Máquinas para la fabricación de redes de pesca, deportivas, protección laboral y para la acuicultura.</p>
           </div>
         </div>
       </div>
@@ -200,31 +203,32 @@
   <section id="contact" style="padding: 5% 0%;">
   <div class="container">
     <div class="well well-sm">
-      <h3><strong>CONTACTO</strong></h3>
+      <h3 class="tit-c"><strong>CONTACTO</strong></h3>
     </div>
   
   <div class="row">
-    <div class="col-md-7">
+    <div class="col-sm-6 col-md-7">
 
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3821.4218147902316!2d-93.17891988575732!3d16.705792126502338!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ecd9f928130d57%3A0xab38da7211a4919a!2sBlvd.+Bonanza%2C+Residencial+Bonanza%2C+29050+Tuxtla+Guti%C3%A9rrez%2C+Chis.!5e0!3m2!1ses-419!2smx!4v1522171373348" width="100%" height="315" frameborder="0" style="border:0" allowfullscreen></iframe>
       </div>
 
-      <div class="col-md-5">
-          <h4><strong>Formulario</strong></h4>
+      <div class="col-sm-6 col-md-5">
+          <h4 class="tit-f"><strong>Formulario</strong></h4>
         <form>
           <div class="form-group">
-            <input type="text" class="form-control" name="" value="" placeholder="Nombre Completo">
+            <input type="text" id="txtNOMBRE" class="form-control" name="" value="" placeholder="Nombre Completo">
           </div>
           <div class="form-group">
-            <input type="email" class="form-control" name="" value="" placeholder="Correo">
+            <input type="email" id="txtCORREO" class="form-control" name="" value="" placeholder="Correo">
           </div>
           <div class="form-group">
-            <input type="tel" class="form-control" name="" value="" placeholder="Teléfono">
+            <input type="tel" id="txtTELEFONO" class="form-control" name="" value="" placeholder="Teléfono">
           </div>
           <div class="form-group">
-            <textarea class="form-control" name="" rows="3" placeholder="Comentario"></textarea>
+            <textarea id="txtCOMENTARIO" class="form-control" name="" rows="3" placeholder="Comentario"></textarea>
           </div>
-          <button class="btn btn-default" type="submit" name="button">
+          <div id="_AJAX_PRE_" ></div>
+          <button class="btn btn-default" type="submit" name="button" onclick="sendCORREO()">
               <i class="far fa-share-square" aria-hidden="true"></i> ENVIAR
           </button>
         </form>
@@ -234,51 +238,58 @@
   <br><br>
   <div class="container">
     <div class="row">
-        <h2>
-            DATOS DE LA EMPRESA</h2> <br><br>
-        <div class="col-md-6">
+        <h2 class="tit-de">DATOS DE LA EMPRESA</h2> <br><br>
+        <div class="col-sm-6 col-md-6">
             <div class="blockquote-box clearfix">
                 <div class="square pull-left">
                    <span class="fas fa-map-marker-alt glyphicon-lg"></span>
                 </div>
-                <h4>
+                <p class="conte-info">
+                  <h4 class="dhct">
                     Dirección</h4>
-                <p>
-                    Priv. Bonanza #430 Col. Residencial Bonanza, Tuxtla Gutierrez, Chiapas. C.P. 29055.
+                  <p class="subtit-dhct">
+                      Priv. Bonanza #430 Col. Residencial Bonanza, Tuxtla Gutierrez, Chiapas. C.P. 29055.
+                  </p>
                 </p>
             </div>
             <div class="blockquote-box blockquote-primary clearfix">
                 <div class="square pull-left">
                     <span class="far fa-clock glyphicon-lg"></span>
                 </div>
-                <h4>
+                <p class="conte-info">
+                  <h4 class="dhct">
                     Horario</h4>
-                <p>
-                    Lunes a Viernes de 9 am a 6 pm
+                  <p class="subtit-dhct">
+                      Lunes a Viernes de 9 am a 6 pm
+                  </p>
                 </p>
             </div>
             
         </div>
-        <div class="col-md-6">
+        <div class="col-sm-6 col-md-6">
             <div class="blockquote-box blockquote-info clearfix">
                 <div class="square pull-left">
                     <span class="far fa-envelope glyphicon-lg"></span>
                 </div>
-                <h4>
+                <p class="conte-info">
+                  <h4 class="dhct">
                     Correo</h4>
-                <p>
-                    info@textilesfulton.com
+                  <p class="subtit-dhct">
+                      info@textilesfulton.com
+                  </p>
                 </p>
             </div>
             <div class="blockquote-box blockquote-warning clearfix">
                 <div class="square pull-left">
                     <span class="fas fa-phone glyphicon-lg"></span>
                 </div>
-                <h4>
+               <p class="conte-info">
+                  <h4 class="dhct">
                     Teléfono</h4>
-                <p>
-                    PENDIENTE
-                </p>
+                  <p class="subtit-dhct">
+                      PENDIENTE
+                  </p>
+               </p>
             </div>
             <!-- <div class="blockquote-box blockquote-danger clearfix">
                 <div class="square pull-left">
@@ -310,5 +321,6 @@
     <script src="js/arriba.js"></script>
     <script src="js/bootstrap.js"></script>
     <script src="js/menu.js"></script>
+    <script src="js/sendCORREO.js"></script>
   </body>
 </html>
